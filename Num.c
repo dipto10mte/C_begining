5bca0f4c2f28dd5e81da4f16686287abdfe4a993
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-10-10 16:23:25 +0600

    Num

---
+++ Num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=50;
+    printf("First value is %d",a);
+    a=-50;
+    printf("Second value is %d",a);
+    a=1000000;
+    printf("Third value is %d",a);
+    a=-1000000;
+    printf("Fourth value is %d",a);
+    return 0;
+}
