56af85084d74634ae680270514b273cb723f83c2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-10-14 19:10:18 +0600

    Chr

---
+++ Chr.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("Enter first letter of your name:");
+    scanf("%c",&a);
+    printf("FIrst latter ofd your name is %c",a);
+    return 0;
+}
