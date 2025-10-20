2f87cc5ac529af2ac5031f78acea9cf6621a640a
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-10-20 22:12:08 +0600

    Com

---
+++ Comment.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    a=20,b=10;
+    /*Adding values
+    to both a,b*/
+    c=a+b;
+    //Doing addition
+    printf("%d+%d=%d",a,b,c);
+    return 0;
+}
