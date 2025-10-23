676e5db736bfd3d057f05633761e581dca1bc3ac
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-10-23 21:48:29 +0600

    Cond

---
+++ Cond1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    n=10;
+    if(n>=0)
+    {
+        printf("Positive number");
+    }
+    else
+    {
+        printf("Negetive number");
+    }
+    return 0;
+}
