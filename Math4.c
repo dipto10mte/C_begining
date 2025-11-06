a9f7de02d397bf79979ddd2a1eff468764351a5b
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-11-06 21:55:05 +0600

    Math

--- Math4.c
+++ Math4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    double a1,a2,b1,b2,c1,c2,x,y;
+    double a1,a2,b1,b2,c1,c2,d,x,y;
     printf("If a1x+b1y=c1 and a2x=b2y=c2. Then enter the values of a1,a2,b1,b2,c1,c2.\n");
     printf("a1=");
     scanf("%lf",&a1);
@@ -15,8 +15,16 @@ int main()
     scanf("%lf",&c1);
     printf("c2=");
     scanf("%lf",&c2);
-    x=(b2*c1-b1*c2)/(a1*b2-a2*b1);
-    y=(a1*c2-a2*c1)/(a1*b2-a2*b1);
-    printf("The result is(x,y)=(%lf,%lf)",x,y);
+    d=a1*b2-a2*b1;
+    x=(b2*c1-b1*c2)/d;
+    y=(a1*c2-a2*c1)/d;
+    if(d==0)
+    {
+        printf("Value of x & y can't be determined.");
+    }
+    else
+    {
+        printf("The result is (x,y)=(%lf,%lf)",x,y);
+    }
     return 0;
 }
