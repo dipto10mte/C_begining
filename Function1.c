314ffef550534e7bdccebc25f33b6d15423772fe
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-11-11 23:20:54 +0600

    Func

---
+++ Function1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int add(double n1,double n2)
+{
+    double sum=n1+n2;
+    return sum;
+}
+int main()
+{
+    double a,b,c;
+    a=b=2.5;
+    c=add(a,b);
+    printf("%lf",c);
+    return 0;
+}
