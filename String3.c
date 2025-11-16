c217e6899d41caabc8b8eb92247b74f3c835d01d
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-11-16 21:43:52 +0600

    Str

---
+++ String3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char country[]={'B','a','n','g','l','a','d','e','s','h','\0'};
+    int i,length;
+    printf("%s\n",country);
+    length=10;
+    for(i=0;i<length;i++)
+    {
+        if(country[i]>=97&&country[i]<=122)
+        {
+            country[i]=country[i]-('a'-'A');
+        }
+    }
+    printf("%s\n",country);
+    return 0;
+}
