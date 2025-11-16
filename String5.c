8a5fa9f57029f3f0c8d68b019cc547789dd1c9f9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-11-16 22:08:13 +0600

    Str

---
+++ String5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int string_length(char str[])
+{
+    int i,length=0;
+    for(i=0;str[i]!='\0';i++)
+    {
+        length++;
+    }
+    return length;
+}
+int main()
+{
+    char country[100];
+    int length;
+    printf("Type a word:");
+    scanf("%s",&country);
+    length=string_length(country);
+    printf("Length:%d\n",length);
+    return 0;
+}
