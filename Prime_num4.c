fccb8b0ab2637a1271f3ee5bf4e64dd37c4c42ab
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-11-21 23:07:46 +0600

    Prn

---
+++ Prime_num4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+const int size=40;
+int ara[size]
+void print_ara()
+{
+    int i;
+    for(i=2;i<size;i++)
+    {
+        printf("%4d",ara[i]);
+    }
+    printf("\n");
+    for(i=2;i<size;i++)
+    {
+        printf("----");
+    }
+    printf(("\n"))
+    for(i=2;i<size;i++)
+    {
+        printf("%4d",i);
+    }
+    printf("\n\n\n");
+}
+void sieve()
+{
+
+}
