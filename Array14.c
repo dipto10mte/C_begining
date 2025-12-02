f6804b5db0d0716cf850e5cebd14f4e6622ba755
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-12-02 21:23:29 +0600

    Array

---
+++ Array14.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int ara[5][5]={{1,2,3,4,5},{10,20,30,40,50},{100,200,300,400,500},{1000,2000,3000,4000,5000},{10000,20000,30000,40000,50000}};
+    int ara2[5][5];
+    int r,c;
+    printf("Content of first array (ara1):\n");
+    for(r=0;r<5;r++)
+    {
+        for(c=0;c<5;c++)
+        {
+            printf("%d ",ara[r][c]);
+        }
+        printf("\n");
+    }
+    for(r=0;r<5;r++)
+    {
+        for(c=0;c<5;c++)
+        {
+            ara2[c][r]=ara[r][c];
+        }
+    }
+    printf("Content of secondary array (ara2):\n");
+    for(r=0;r<5;r++)
+    {
+        for(c=0;c<5;c++)
+        {
+            printf("%d ",ara2[r][c]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
