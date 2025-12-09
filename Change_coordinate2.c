5138ffe8758c3ea2c065726ce7c6585bd6bc7777
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-12-09 17:17:04 +0600

    CC

--- Change_coordinate2.c
+++ Change_coordinate2.c
@@ -1,23 +1,48 @@
 #include<stdio.h>
 int main()
 {
-    int c,i,j,x,y;
-    printf("Enter the co-ordinates of blocked cells: ");
-    scanf("%d %d",&i,&j);
-    printf("Now enter the position co-ordinates: ");
-    scanf("%d %d",&x,&y);
+    int i,j,n,x,y,grid[i][j];
+    char c;
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            grid[i][j]=1;
+        }
+    }
+    printf("Please enter the blocked cells: ");
+    scanf("%d",&n);
+    printf("Please enter initial position: ");
+    scanf("%d %d", &x, &y);
     while(1)
     {
-        scanf("%c",&c)
-        if(c=='E')
+        scanf(" %c", &c);
+        if(c == 'S')
         {
             break;
         }
-        else if(c=='')
+        else if(c == 'U')
         {
-
+            x--;
         }
-        printf("Final position: %d %d",x,y);
+        else if(c == 'D')
+        {
+            x++;
+        }
+        else if(c == 'L')
+        {
+            y--;
+        }
+        else if(c == 'R')
+        {
+            y++;
+        }
+        else
+        {
+            printf("Invalid input\n");
+        }
+        printf("Final position: (%d,%d)\n", x, y);
     }
+    printf("Final position: (%d,%d)\n", x, y);
     return 0;
 }
