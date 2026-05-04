f5fbe5a9823700131e49aaa2d416e085818c92cd
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-04 12:42:12 +0600

    Update

--- Structure7.c
+++ Structure7.c
@@ -12,3 +12,20 @@ typedef struct{
     char grade[3];
 } studenttype;
 
+void calculate_grade(studenttype *s, int mark)
+{
+    if (mark>=80)
+        strcpy(s->grade, "A+");
+    else if(mark>=70)
+        strcpy(s->grade, "A");
+    else if(mark>=60)
+        strcpy(s->grade, "A-");
+    else if(mark>=50)
+        strcpy(s->grade, "B");
+    else if(mark>=40)
+        strcpy(s->grade, "C");
+    else
+        strcpy(s->grade, "F");
+
+}
+
