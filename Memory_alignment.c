5557a5a3afc3a992c17a21ae64c01c0d0662a126
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-07 23:28:06 +0600

    Update

--- Memory_alignment.c
+++ Memory_alignment.c
@@ -14,7 +14,12 @@ struct s1{
 
  int main()
  {
+    printf("char size : %lu bytes\n",sizeof(char));
+    printf("int size : %lu bytes\n",sizeof(int));
+    printf("double size : %lu bytes\n",sizeof(double));
 
+    printf("s1 size : %lu bytes\n",sizeof(struct s1));
+    printf("s2 size : %lu bytes\n",sizeof(struct s2));
 
-     return 0;
+    return 0;
  }
