d16062b22f4cb1139614f6369ac5ca0391b5264c
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-08 12:24:16 +0600

    Update

--- Pointer8.c
+++ Pointer8.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 in main()
 {
+    int *marks;
+    int i,n;
+    printf("Please enter the number of student: ");
+    scanf("%d",&n);
+
+    //now allocate memory
+    marks=int(*)malloc(sizeof(int)*n);
 
     return 0;
 }
