d832b121d53d7a80cb98baa83761f5920026d0e1
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-09 15:19:57 +0600

    Update

--- Pointer9.c
+++ Pointer9.c
@@ -15,6 +15,15 @@ int main()
         return 1;
     }
 
+    printf("Enter the marks for each student:\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&marks[i]);
+    printf("Now here you can see the values:\n");
+    for(i=0;i<n;i++)
+        printf("%d\n",marks[i]);
+
+    //now free the memory
+    free(marks);
 
     return 0;
 }
