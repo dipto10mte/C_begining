bbd3c6209fad66cd2e332c1b8020300948b040c8
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-09 15:19:45 +0600

    Update

--- Pointer9.c
+++ Pointer9.c
@@ -3,6 +3,17 @@
 
 int main()
 {
+    int *marks;
+    int i,n;
+    printf("Please enter the number of student: ");
+    scanf("%d",&n);
+
+    //allocate memory
+    marks=(int*)malloc(sizeof(int)*n);
+    if(marks==NULL){
+        printf("Memory allocation failed for marks\n");
+        return 1;
+    }
 
 
     return 0;
